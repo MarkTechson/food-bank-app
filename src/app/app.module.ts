06c683ae1845302121b7b0f9ac9c74d354bfc0c2
Mark Thompson <markathompson@google.com>
2020-10-15 21:18:37 -0500

    Initial commit
    Add the functioning food bank app, including assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FoodBankLocationComponent } from './food-bank-location/food-bank-location.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodBankLocationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/food-bank-location/food-bank-location.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-food-bank-location',
+  templateUrl: './food-bank-location.component.html',
+  styleUrls: ['./food-bank-location.component.css']
+})
+export class FoodBankLocationComponent implements OnInit {
+
+  @Input() details;
+  @Output() detailsChange = new EventEmitter();
+
+  constructor() { }
+
+  notifyDetailsChange() {
+    this.detailsChange.emit(this.details);
+  }
+  ngOnInit(): void {
+  }
+}
